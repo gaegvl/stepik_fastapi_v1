0b7b3a0a8f3e9b9498dc23cebde3b398e62911b5
Egor Gavrilov <Egor4it@yandex.ru>
2026-04-19 12:45:13 +0500

    добавление столбцов и реализация поиска

--- app/models/products.py
+++ app/models/products.py
@@ -1,27 +1,85 @@
+import datetime
 from decimal import Decimal
-from sqlalchemy import String, Boolean, Numeric, Integer, ForeignKey
+from sqlalchemy import (
+    String,
+    Boolean,
+    Numeric,
+    Integer,
+    ForeignKey,
+    DateTime,
+    Computed,
+    Index,
+)
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import TSVECTOR
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from app.models.categories import Category
+    from app.models.users import User
+    from app.models.reviews import Review
+    from app.models.cart_items import CartItem
+    from app.models.orders import OrderItem
+
 
 from app.database import Base
+
 
 class Product(Base):
     __tablename__ = "products"
 
-    id:Mapped[int] = mapped_column(primary_key=True)
-    name:Mapped[str] = mapped_column(String(100), nullable=False)
-    description:Mapped[str|None] = mapped_column(String(500), nullable=True)
-    price:Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
-    image_url:Mapped[str|None] = mapped_column(String(200), nullable=True)
-    stock:Mapped[int] = mapped_column(Integer, nullable=False)
-    is_active:Mapped[bool] = mapped_column(Boolean, default=True)
-    rating:Mapped[float] = mapped_column(Numeric(6, 5), default=1.0, nullable=True)
-    
-    category_id:Mapped[int] = mapped_column(Integer, ForeignKey("categories.id"), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str | None] = mapped_column(String(500), nullable=True)
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+    image_url: Mapped[str | None] = mapped_column(String(200), nullable=True)
+    stock: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    rating: Mapped[float] = mapped_column(Numeric(6, 5), default=0.0, nullable=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    upddated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
 
-    category:Mapped["Category"] = relationship("Category", back_populates="products")
+    category_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("categories.id"), nullable=False
+    )
 
-    seller_id:Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    category: Mapped["Category"] = relationship("Category", back_populates="products")
 
-    seller:Mapped["User"] = relationship("User", back_populates="products")
-    
-    reviews:Mapped[list["Review"]] = relationship("Review", back_populates="products")
+    seller_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("users.id"), nullable=False
+    )
+
+    seller: Mapped["User"] = relationship("User", back_populates="products")
+
+    reviews: Mapped[list["Review"]] = relationship("Review", back_populates="products")
+
+    cart_items: Mapped[list["CartItem"]] = relationship(
+        "CartItem", back_populates="product", cascade="all, delete-orphan"
+    )
+
+    tsv: Mapped[TSVECTOR] = mapped_column(
+        TSVECTOR,
+        Computed(
+            """
+            setweight(to_tsvector('english', coalesce(name, '')), 'A')
+            || 
+            setweight(to_tsvector('english', coalesce(description, '')), 'B')
+            """,
+            persisted=True,
+        ),
+        nullable=True,
+    )
+
+    order_items: Mapped[list["OrderItem"]] = relationship(
+        "OrderItem", back_populates="product"
+    )
+    __table_args__ = (Index("ix_products_tsv_gin", "tsv", postgresql_using="gin"),)
--- app/routers/products.py
+++ app/routers/products.py
@@ -1,23 +1,64 @@
-from fastapi import APIRouter, Depends, Body, Path, status, HTTPException
-from sqlalchemy import select, update, and_
-from typing import Annotated
+from operator import eq, ge, le
+from fastapi import APIRouter, Depends, Body, Path, status, HTTPException, Query
+from sqlalchemy import desc, select, update, and_
+from typing import Annotated, Any
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload, with_loader_criteria
+from sqlalchemy.sql import func
 
 from app.auth import get_current_seller
 from app.models import Product as ProductModel, Category
 from app.models.users import User as UserModel
 from app.models.reviews import Review as ReviewsModel
-from app.schemas import ProductCreate, Product as ProductSchema, Review as ReviewSchema
+from app.schemas import (
+    ProductCreate,
+    Product as ProductSchema,
+    ProductFilter,
+    ProductList,
+    Review as ReviewSchema,
+)
 from app.db_depends import get_db
 
 
 # Создаём маршрутизатор для товаров
 router = APIRouter(
-    prefix="/products", 
+    prefix="/products",
     tags=["products"],
 )
+
+
+FILTERS = [
+    ("category_id", ProductModel.category_id, eq),
+    ("min_price", ProductModel.price, ge),
+    ("max_price", ProductModel.price, le),
+    ("seller_id", ProductModel.seller_id, eq),
+    ("created_at", ProductModel.created_at, le),
+]
+
+
+def build_filters(filters: ProductFilter) -> list:
+    all_filters = [ProductModel.is_active]
+
+    if (
+        filters.min_price is not None
+        and filters.max_price is not None
+        and filters.min_price > filters.max_price
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="min_price не может быть больше max_price",
+        )
+
+    for param_name, column, op in FILTERS:
+        value = getattr(filters, param_name, None)
+        if value is not None:
+            all_filters.append(op(column, value))
+    if filters.in_stock is not None:
+        all_filters.append(
+            ProductModel.stock > 0 if filters.in_stock else ProductModel.stock == 0
+        )
+    return all_filters
 
 
 async def category_by_id(
@@ -36,8 +77,9 @@ async def category_by_id(
 
 
 async def product_by_id(
-    product_id: Annotated[int, Path()], db: Annotated[AsyncSession, Depends(get_db)],
-    current_user:Annotated[UserModel, Depends(get_current_seller)]
+    product_id: Annotated[int, Path()],
+    db: Annotated[AsyncSession, Depends(get_db)],
+    current_user: Annotated[UserModel, Depends(get_current_seller)],
 ) -> ProductModel:
     result = await db.scalars(
         select(ProductModel).where(
@@ -46,7 +88,10 @@ async def product_by_id(
     )
     product = result.first()
     if product.seller_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not the owner of this product")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not the owner of this product",
+        )
     if not product:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -55,23 +100,66 @@ async def product_by_id(
     return product
 
 
-@router.get("/", response_model=list[ProductSchema], status_code=status.HTTP_200_OK)
+@router.get("/", response_model=ProductList, status_code=status.HTTP_200_OK)
 async def get_all_products(
+    page: Annotated[int, Query(ge=1)],
     db: Annotated[AsyncSession, Depends(get_db)],
-) -> list[ProductSchema]:
+    filters: Annotated[ProductFilter, Depends()],
+    page_size: int = Query(3, ge=1, le=100),
+    search: str | None = Query(
+        None, min_length=1, description="Поиск по названию товара"
+    ),
+) -> dict[str, Any]:
     """
     Возвращает список всех товаров.
     """
-    result = await db.scalars(select(ProductModel).where(ProductModel.is_active))
-    products: list[ProductModel] = result.all()
-    return products
+
+    all_filters = build_filters(filters)
+
+    rank_colummn = None
+
+    if search:
+        search_value = search.strip()
+        if search_value:
+            ts_query = func.websearch_to_tsquery("english", search_value)
+            all_filters.append(ProductModel.tsv.op("@@")(ts_query))
+            rank_colummn = func.ts_rank_cd(ProductModel.tsv, ts_query).label("rank")
+
+    total = (
+        await db.scalar(
+            select(func.count()).select_from(ProductModel).where(and_(*all_filters))
+        )
+        or 0
+    )
+
+    if rank_colummn is not None:
+        product_stmt = (
+            select(ProductModel, rank_colummn)
+            .where(and_(*all_filters))
+            .order_by(desc(rank_colummn), ProductModel.id)
+            .offset((page - 1) * page_size)
+            .limit(page_size)
+        )
+        product = await db.execute(product_stmt)
+        rows = product.all()
+        items = [row[0] for row in rows]
+    else:
+        result = await db.scalars(
+            select(ProductModel)
+            .where(and_(*all_filters))
+            .order_by(ProductModel.id)
+            .offset((page - 1) * page_size)
+            .limit(page_size)
+        )
+        items: list[ProductModel] = result.all()
+    return {"items": items, "total": total, "page": page, "page_size": page_size}
 
 
 @router.post("/", response_model=ProductSchema, status_code=status.HTTP_201_CREATED)
 async def create_product(
     product: Annotated[ProductCreate, Body()],
     db: Annotated[AsyncSession, Depends(get_db)],
-    current_user:Annotated[UserModel, Depends(get_current_seller)]
+    current_user: Annotated[UserModel, Depends(get_current_seller)],
 ) -> ProductSchema:
     """
     Создаёт новый товар.
@@ -104,11 +192,31 @@ async def get_products_by_category(
     products = result.all()
     return products
 
-@router.get("/{product_id}/reviews", response_model=list[ReviewSchema], status_code=status.HTTP_200_OK)
-async def get_reviews_by_product_id(product_id: Annotated[int, Path()], db:Annotated[AsyncSession, Depends(get_db)]) -> list[ReviewSchema]:
-    product = await db.scalar(select(ProductModel).options(selectinload(ProductModel.reviews), with_loader_criteria(ReviewsModel, ReviewsModel.is_active)).where(ProductModel.id == product_id, ProductModel.is_active,))
+
+@router.get(
+    "/{product_id}/reviews",
+    response_model=list[ReviewSchema],
+    status_code=status.HTTP_200_OK,
+)
+async def get_reviews_by_product_id(
+    product_id: Annotated[int, Path()], db: Annotated[AsyncSession, Depends(get_db)]
+) -> list[ReviewSchema]:
+    product = await db.scalar(
+        select(ProductModel)
+        .options(
+            selectinload(ProductModel.reviews),
+            with_loader_criteria(ReviewsModel, ReviewsModel.is_active),
+        )
+        .where(
+            ProductModel.id == product_id,
+            ProductModel.is_active,
+        )
+    )
     if not product:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found or inactive")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Product not found or inactive",
+        )
 
     return product.reviews
 
@@ -138,12 +246,14 @@ async def update_product(
     """
     Обновляет товар по его ID.
     """
-    
+
     await category_by_id(new_product.category_id, db)
     await db.execute(
         update(ProductModel)
         .where(ProductModel.id == product.id)
-        .values(**new_product.model_dump(),)
+        .values(
+            **new_product.model_dump(),
+        )
     )
     await db.commit()
     await db.refresh(product)
